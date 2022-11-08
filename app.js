918f7bc548a9b103753b483271e414cbf6ec9a18
alinbrg <alina.bregvadze@gmail.com>
2022-11-08 22:17:14 +0400

    js variables

---
+++ app.js
@@ -0,0 +1,99 @@
+/*hello + 1;
+
+console.log("hello");
+*/
+
+// console.log("after comment");
+
+// var, let, const
+
+myName = "Alina";
+
+var myName1 = "Alina";
+
+// console.log(myName, myName1);
+
+let userName = "Tea";
+let $username = "Tea";
+let _userNAME = "Tea";
+// console.log(userName);
+
+userName = "Tiko";
+userName = 10;
+
+// console.log(userName);
+const userId = "asbcagv121893287";
+
+const userBirthYear = 1900.5;
+
+const myNumber = 10;
+const myString = "some text";
+const myBoolTrue = true;
+const myBoolFalse = false;
+
+let myUndefined = undefined;
+let myNull = null;
+
+// console.log(typeof myNumber, "myNumber");
+// console.log(typeof myString, "myString");
+// console.log(typeof myBoolTrue, "myBoolTrue");
+// console.log(typeof myBoolFalse, "myBoolFalse");
+// console.log(typeof myUndefined, "myUndefined");
+// console.log(typeof myNull, "myNull");
+
+const someText = 'someone\'s "error" error';
+
+// console.log(someText);
+
+const num1 = 10;
+const num2 = 20;
+const num3 = 3;
+
+const addNumbers = num1 + num2;
+
+const multNumbers = num1 * num2;
+const substNumbers = num1 - num2;
+const divNumbers = num1 / num2;
+const moduloNumbers = num1 % num3; //ნაშთი
+const powerNUmbers = num1 ** num3; //ხარისხში აყვანა NaN not a number
+
+// console.log(
+// 	addNumbers,
+// 	"addNumbers",
+// 	multNumbers,
+// 	"multNumbers",
+// 	substNumbers,
+// 	"substNumbers",
+// 	divNumbers,
+// 	"divNumbers",
+// 	moduloNumbers,
+// 	"moduloNumbers",
+// 	powerNUmbers,
+// 	"powerNUmbers"
+// );
+
+// const someText2 = "smart academy";
+const someText2 = "smart academy";
+const groupNumber = 18;
+const studentNumber = 7;
+
+let text1 = someText2 + groupNumber + studentNumber;
+
+// console.log(text1, "someText2 + groupNumber + studentNumber");
+
+text1 = groupNumber + studentNumber + someText2;
+
+// console.log(text1, "groupNumber + studentNumber + someText2");
+
+text1 = studentNumber + someText2 + groupNumber;
+
+// console.log(text1, "studentNumber + someText2 + groupNumber");
+
+const text2 = "hello";
+const text3 = " user name";
+
+// console.log("some  text " + (5 + 3) + text2 + text3);
+
+const text4 = `some text 5+3 ${5 + 3} text2 ${text2} ${text3}`;
+
+// console.log(text4);
